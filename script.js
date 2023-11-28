185ade4439e9f4049f767b6978dbcbdf3dae7b4c
MatthiasKebede <msk9862@nyu.edu>
2023-11-28 16:42:18 +0400

    11/28 afternoon

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let vidArea = document.getElementById("vid-area");
 let movie = document.getElementById("movie");
 let textArea = document.getElementById("text-area");
 let screenText = document.getElementById("text-content");
+let beginText = document.getElementById("begin");
 let respArea = document.getElementById("response-area");
 let respPrompt = document.getElementById("response-prompt");
 let respField = document.getElementById("response-field");
@@ -18,8 +19,10 @@ let contactArea = document.getElementById("contact-area");
 
 
 // // Event listeners
-vidArea.addEventListener("click", function() {window.setInterval(appendText,200)});
+document.body.addEventListener("click", function() {window.setInterval(appendText,200)});
+movie.addEventListener("play", function() {textArea.style.display = "none"});
 movie.addEventListener("ended", inputPopup);
+// respField.addEventListener("keyup", function(event) {if(event.key === "enter") {}})
 respSubmit.addEventListener("click", inputPopout);
 
 
@@ -31,19 +34,30 @@ function appendText() {
         i++;
     }
     else {
-        window.clearInterval()
-        vidArea.removeEventListener("click", function() {window.setInterval(appendText,200)});
-        vidArea.addEventListener("click", function() {
+        document.body.removeEventListener("click", function() {window.setInterval(appendText,200)});
+        window.clearInterval();
+        document.body.addEventListener("click", function() {
             screenText.classList.add("fade");
+            beginText.classList.add("fade");
             window.setTimeout(playMovie, 4000);
         });
+        window.setTimeout(function () {beginText.style.display = "block"}, 1000);
     }
 }
 function playMovie() {
-    movie.play();
+    document.body.removeEventListener("click", function() {screenText.classList.add("fade");beginText.classList.add("fade");window.setTimeout(playMovie, 4000);});
+    if (movie.style.display != "none") {
+        movie.play();
+    }
 }
 function inputPopup() {
-    respArea.style.display = "block";
+    movie.classList.add("fade");
+    window.setTimeout(function() {
+        movie.style.display = "none"; 
+        movie.setAttribute("src","./assets/movie.mp4"); 
+        movie.volume = 0;
+        respArea.style.display = "block"
+    }, 2000);    
 }
 function inputPopout() {
     respArea.style.display = "none";
